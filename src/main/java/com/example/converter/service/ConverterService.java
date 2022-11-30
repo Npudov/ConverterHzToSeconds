34bb631ec2ab817d9a096d8ffce2788e9c1cfe75
somagic2 <nik.p008@gmail.com>
2022-11-30 22:53:13 +0300

    Create validation functions

--- src/main/java/com/example/converter/service/ConverterService.java
+++ src/main/java/com/example/converter/service/ConverterService.java
@@ -3,6 +3,10 @@ package com.example.converter.service;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class ConverterService {
 
@@ -18,5 +22,27 @@ public class ConverterService {
             return null;
         }
         return String.valueOf(1 / Double.parseDouble(timeData));
+    }
+
+    public static String validData(String inputData) {
+        Pattern pattern = Pattern.compile("\\d+.\\d+");
+        Matcher matcher = pattern.matcher(inputData);
+        String answer = null;
+        if (!matcher.matches()) {
+            answer = "Invalid Data!";
+        }
+        return answer;
+    }
+
+    public static String validArrayData(List<String> inputArrayData) {
+        Pattern pattern = Pattern.compile("\\d+.\\d+");
+        String answer = null;
+        for (String element: inputArrayData) {
+            Matcher matcher = pattern.matcher(element);
+            if (!matcher.matches()) {
+                answer = "Invalid Data!";
+            }
+        }
+        return answer;
     }
 }
