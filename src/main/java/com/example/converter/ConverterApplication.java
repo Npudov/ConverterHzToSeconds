5d4e64bf232630a3bdf4ef4884d0668a98c77f71
somagic2 <nik.p008@gmail.com>
2022-12-03 12:08:53 +0300

    Add conctructor to convertController and edit jakarta to javax

--- src/main/java/com/example/converter/ConverterApplication.java
+++ src/main/java/com/example/converter/ConverterApplication.java
@@ -2,6 +2,7 @@ package com.example.converter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ConverterApplication {
 
