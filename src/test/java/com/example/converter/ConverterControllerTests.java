36b28206adff7585d78b364ce31f6d2b93389b08
somagic2 <nik.p008@gmail.com>
2022-12-10 21:50:03 +0300

    Create tests

---
+++ src/test/java/com/example/converter/ConverterControllerTests.java
@@ -0,0 +1,66 @@
+package com.example.converter;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.*;
+
+//import static io.restassured.RestAssured.given;
+
+//@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class ConverterControllerTests {
+
+    @BeforeAll
+    public static void setup() throws Exception {
+        //RestAssured.baseURI = "https://localhost:";
+        RestAssured.port = 8080;
+    }
+
+    @Test
+    public void getRequestHzToTime() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .param("frequency", 1000)
+                .when()
+                .get("/HzToTime")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals("0.001", response.getBody().asString());
+    }
+
+    @Test
+    public void getRequestTimeToHz() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .param("time", 500)
+                .when()
+                .get("/TimeToHz")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals("0.002", response.getBody().asString());
+    }
+
+    @Test
+    public void getErrorRequestHzToTime() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .param("frequency", 0)
+                .when()
+                .get("/HzToTime")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(400, response.statusCode());
+        Assertions.assertEquals("Invalid Data!", response.getBody().asString());
+    }
+}
